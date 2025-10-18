d1da2ab76842fe50154354e18507dc1ee1fc1de4
sumas7 <smlcurreli@gmail.com>
2025-10-18 18:36:36 +0200

    Update parrot-trouble.ml

--- level-1/parrot-trouble/parrot-trouble.ml
+++ level-1/parrot-trouble/parrot-trouble.ml
@@ -1,2 +1,5 @@
-let map parrot_trouble a b = function
-  | Some a -> Some a;;
+let parrot_trouble talking time = match (talking, time) with
+  | (_, -1) -> None
+  | (_, 24) -> None
+  | (false, _) -> Some false
+  | (true, _) -> Some ((0 < time && time < 7) || (20 < time && time < 23));;
