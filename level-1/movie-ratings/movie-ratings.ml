959bfb978faa764f4f09225423b36ec429909f94
sumas7 <smlcurreli@gmail.com>
2025-10-19 17:04:26 +0200

    now works on vs code's terminal without crashing

--- level-1/movie-ratings/movie-ratings.ml
+++ level-1/movie-ratings/movie-ratings.ml
@@ -2,16 +2,8 @@ let movie_ratings vote1 vote2 vote3 =
   if((0 < vote1 && vote1 < 6) && (0 < vote2 && vote2 < 6) && (0 < vote3 && vote3 < 6)) then
     match (vote1, vote2, vote3) with
       | (5, 5, 5) -> "Masterpiece"
-      | (4, 5, 5) -> "Highly Recommended"
-      | (5, 4, 5) -> "Highly Recommended"
-      | (5, 5, 4) -> "Highly Recommended"
-      | (4, 4, 4) -> "Recommended"
-      | (3, 4, 4) -> "Recommended"
-      | (4, 3, 4) -> "Recommended"
-      | (4, 4, 3) -> "Recommended"
-      | (3, 4, 5) -> "Recommended"
-      | (3, 5, 4) -> "Recommended"
-      | (3, 5, 5) -> "Recommended"
+      | (4, 5, 5) | (5, 4, 5) | (5, 5, 4) -> "Highly Recommended"
+      | (4, 4, 4) | (3, 4, 4) | (4, 3, 4) | (4, 4, 3) | (3, 4, 5) | (3, 5, 4) | (3, 5, 5) -> "Recommended"
       | _ -> "Mixed Reviews"
   else
     "ERROR: vote out of range";;
