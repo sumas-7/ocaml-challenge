108bbbedf79c0471e3d268e30d23cac01371a056
Samuele Curreli <smlcurreli@gmail.com>
2025-10-23 11:11:42 +0200

    Update head-or-tail.ml

--- level-1/head-or-tail/head-or-tail.ml
+++ level-1/head-or-tail/head-or-tail.ml
@@ -1,3 +1,5 @@
 let is_even x = (x mod 2) = 0;;
 
-let hot = if is_even (Random.int(256000)) then "head" else "tail";;
+Random.self_init();;
+
+let hot () = if is_even (Random.int(256000)) then "head" else "tail";;
